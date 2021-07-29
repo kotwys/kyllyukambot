209740c585e18aecb9288e108ba6b15d929af81b
kotwys <52920928+kotwys@users.noreply.github.com>
2021-07-29 20:24:00 +0400

    Move Telegram logic to separate namespace

--- src/kyllyukambot/core.clj
+++ src/kyllyukambot/core.clj
@@ -1,53 +1,8 @@
 (ns kyllyukambot.core
-  (:require [kyllyukambot.api :as api]
-            [kyllyukambot.polling :refer [start]]
-            [clojure.core.async :refer [<!!]]
-            [clojure.core.match :refer [match]]
-            [clojure.string :as str]
-            [selmer.parser :refer [render-file]]
-            [telegrambot-lib.core :as tbot])
+  (:require [clojure.core.async :refer [<!!]]
+            [kyllyukambot.handler :refer [bot handle]]
+            [kyllyukambot.polling :refer [start]])
   (:gen-class))
-
-(def bot (tbot/create))
-
-(defn lookup [word lang]
-  (if (nil? word)
-    {:text (render-file "word-not-specified.md" {:lang lang})}
-    (let [data (api/get-defs word (keyword lang))]
-      (if (seq data)
-        {:text (str/join "\n\n" data)
-         :parse_mode "HTML"}
-        {:text (render-file "not-found.md" {:lang lang})
-         :parse_mode "Markdown"}))))
-
-(defn handle [update]
-  (match [update]
-    [{:message {:text msg, :chat {:id id}}}]
-    (if-let [tokens (seq (str/split msg #"\s+"))]
-      (case (-> tokens first (str/split #"@") first)
-        "/start"
-        {:method "sendMessage"
-         :chat_id id
-         :text (render-file "start.md" {})
-         :parse_mode "Markdown"}
-        
-        "/help"
-        {:method "sendMessage"
-         :chat_id id
-         :text (render-file "help.md" {})
-         :parse_mode "Markdown"}
-        
-        "/udm"
-        (merge (lookup (second tokens) "udm")
-               {:method "sendMessage"
-                :chat_id id})
-
-        "/ru"
-        (merge (lookup (second tokens) "ru")
-               {:method "sendMessage"
-                :chat_id id})))
-
-    :else nil))
 
 (defn -main [& args]
   (when (nil? (:bot-token bot))
---
+++ src/kyllyukambot/handler.clj
@@ -0,0 +1,47 @@
+(ns kyllyukambot.handler
+  (:require [clojure.core.match :refer [match]]
+            [clojure.string :as str]
+            [selmer.parser :refer [render-file]]
+            [telegrambot-lib.core :as tbot]
+            [kyllyukambot.api :as api]))
+
+(def bot (tbot/create))
+
+(defn lookup [word lang]
+  (if (nil? word)
+    {:text (render-file "word-not-specified.md" {:lang lang})}
+    (let [data (api/get-defs word (keyword lang))]
+      (if (seq data)
+        {:text (str/join "\n\n" data)
+         :parse_mode "HTML"}
+        {:text (render-file "not-found.md" {:lang lang})
+         :parse_mode "Markdown"}))))
+
+(defn handle [update]
+  (match [update]
+    [{:message {:text msg, :chat {:id id}}}]
+    (if-let [tokens (seq (str/split msg #"\s+"))]
+      (case (-> tokens first (str/split #"@") first)
+        "/start"
+        {:method "sendMessage"
+         :chat_id id
+         :text (render-file "start.md" {})
+         :parse_mode "Markdown"}
+        
+        "/help"
+        {:method "sendMessage"
+         :chat_id id
+         :text (render-file "help.md" {})
+         :parse_mode "Markdown"}
+        
+        "/udm"
+        (merge (lookup (second tokens) "udm")
+               {:method "sendMessage"
+                :chat_id id})
+
+        "/ru"
+        (merge (lookup (second tokens) "ru")
+               {:method "sendMessage"
+                :chat_id id})))
+
+    :else nil))
